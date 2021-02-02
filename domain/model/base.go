00f43e9c368dce24f1fa914562a9cf185c55e7ea
Thales Gabriel <thales.anjos@bemind.com.br>
2021-02-02 13:41:32 -0300

    Suporte ao servidor de grpc

--- domain/model/base.go
+++ domain/model/base.go
@@ -6,12 +6,12 @@ import (
 	"github.com/asaskevich/govalidator"
 )
 
-type Base struct {
-	ID        string    `json:"id" valid:"uuid"`
-	CreatedAt time.Time `json:"created_at" valid:"-"`
-	UpdatedAt time.Time `json:"updated_at" valid:"-"`
-}
-
 func init() {
 	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+type Base struct {
+	ID        string    `json:"id" gorm:"type:uuid;primary_key" valid:"uuid"`
+	CreatedAt time.Time `json:"created_at" valid:"-"`
+	UpdatedAt time.Time `json:"updated_at" valid:"-"`
 }
